e667d14329caf239220a6907e7a298d2b9369fd3
rayfanaqbil <rayfana09@gmail.com>
2023-01-12 01:39:55 +0700

    rayfan

--- simple_upload/database.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS 'siswa' (
-    'id' int(11) NOT NULL AUTO_INCREMENT,
-    'nama' varchar(255) collate utf8mb4_general_ci NOT NULL,
-    'foto' text COLLATE utf8mb4_general_ci NOT NULL,
-    
-)ENGINE=MyISAM  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci AUTO_INCREMENT=100001 ;
-
-INSERT INTO 'siswa' ('id', 'nama', 'foto')
